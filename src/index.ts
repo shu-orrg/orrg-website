6ebcc0fba8034186cb20003c6c7ba189e925453a
Matthew Ryall <matthew@mexico86.co.uk>
2020-01-17 10:06:13 +0000

    Move front page images to Cloudinary

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,41 @@
-const imageBase = "https://ucarecdn.com/";
+const imageBase = "https://res.cloudinary.com/mexico86/image/upload/";
+const transformation = "c_lfill,g_center,h_550,w_1200,x_0";
+
 const imageList = [
-  "85bb1000-38da-40f6-8c1a-dbe81c17bd17/-/scale_crop/1200x550/center/",
-  "e3a037b1-c5a7-49a4-a0fe-25d9848712e8/-/scale_crop/1200x550/center/",
-  "721d3caa-8beb-4d91-92fa-0a88a07ac75d/-/scale_crop/1200x550/center/",
-  "6c243a96-337c-49b6-a049-3e066c96756f/-/scale_crop/1200x550/center/",
-  "5fdafb4a-b165-42c0-9b23-0ed6b90161b9/-/scale_crop/1200x550/center/",
-  "730331c6-33ca-46ea-83b8-34778c2c99ae/-/scale_crop/1200x550/center/",
-  "4339fd3a-030f-4321-9ccc-6ddb32365fbb/-/scale_crop/1200x550/center/",
-  "0b4e5b17-701a-46cb-8682-438a15cef7b6/-/scale_crop/1200x550/center/",
-  "41e509c5-16cd-4dd9-a7fc-99942a3d4188/-/scale_crop/1200x550/center/",
-  "8aedf866-af75-4b3d-93fc-3fa1283c2458/-/scale_crop/1200x550/center/",
-  "9be1e568-84b6-4a2f-82fb-0899fc61fe32/-/scale_crop/1200x550/center/",
-  "5421420c-daa0-4751-86c2-bb13adb17a1f/-/scale_crop/1200x550/center/",
-  "7e7b7911-1dd2-4196-8b62-dd768ccdb00a/-/scale_crop/1200x550/center/",
-  "93faf9e5-602d-43ee-af88-2d86e98b710b/-/scale_crop/1200x550/center/",
-  "2d6b9bea-57df-496b-b515-0c45ec0503da/-/scale_crop/1200x550/center/",
-  "4e431b2f-af9d-4a6c-b913-811ed26b76f3/-/scale_crop/1200x550/center/"
+  "/v1579253276/ORRG/BMX_cityscape_Sheffield_The_Outdoor_City_dih19k.jpg",
+  "/v1579253275/ORRG/Bouldering_2._Sheffield_The_Outdoor_City_utjbxz.jpg",
+  "/v1579253275/ORRG/Bouldering_1_Sheffield_The_Outdoor_City_bglz5p.jpg",
+  "/v1579253275/ORRG/Bouldering_3_Sheffield_The_Outdoor_City_ww6qyf.jpg",
+  "/v1579253274/ORRG/Bouldering_4_Sheffield_The_Outdoor_City_dbzyhp.jpg",
+  "/v1579253274/ORRG/Eating_out_The_Riverside_pub_Sheffield_The_Outdoor_City_r7aor7.jpg",
+  "/v1579253273/ORRG/Fell_running_out_to_the_city_limits_Sheffield_The_Outdoor_City_zv5frl.jpg",
+  "/v1579253273/ORRG/Indoor_climbing_1_Sheffield_The_Outdoor_City_ezsjhk.jpg",
+  "/v1579253272/ORRG/Paddle_boarding_on_the_River_Don_Sheffield_The_Outdoor_City_nsdte4.jpg",
+  "/v1579253272/ORRG/Indoor_climbing_2_Sheffield_The_Outdoor_City_vzekap.jpg",
+  "/v1579253272/ORRG/Paddle_boarding_on_the_River_Don_2_Sheffield_The_Outdoor_City_m8lap5.jpg",
+  "/v1579253271/ORRG/Mountain_biking_in_Sheffield_The_Outdoor_City_lguhwv.jpg",
+  "/v1579253271/ORRG/Mountain_biker_Sheffield_The_Outdoor_City_rbixgu.jpg",
+  "/v1579253270/ORRG/Riding_inner_city_trails_1_Parkwood_Springs_Sheffield_The_Outdoor_City_txwkxf.jpg",
+  "/v1579253270/ORRG/Planning_your_adventure_Sheffield_The_Outdoor_City_be1yym.jpg",
+  "/v1579253270/ORRG/Road_cycling_Sheffield_The_Outdoor_City_dg9jya.jpg",
+  "/v1579253269/ORRG/Riding_inner_city_trails_2_Parkwood_Springs_Sheffield_The_Outdoor_City_rv9pjr.jpg",
+  "/v1579253269/ORRG/Trad_rock_climbing_2_Sheffield_The_Outdoor_City_e1ijiy.jpg",
+  "/v1579253268/ORRG/Victoria_Quays_Sheffield_The_Outdoor_City_wbfq7y.jpg",
+  "/v1579253268/ORRG/Urban_running_Winter_Garden_Sheffield_The_Outdoor_City_o4lubh.jpg",
+  "/v1579253267/ORRG/Wild_Swimming_Sheffield_The_Outdoor_City_v9nint.jpg",
+  "/v1579253267/ORRG/Yoga_1_Winter_Garden_Sheffield_The_Outdoor_City_rglnle.jpg",
+  "/v1579253267/ORRG/Yoga_2_Winter_Garden_Sheffield_The_Outdoor_City_xgiysl.jpg",
+  "/v1579253266/ORRG/Trad_rock_climbing_Sheffield_The_Outdoor_City_j4lbop.jpg"
 ];
 
 const imageIndex = new Date().getDate() % imageList.length;
 
 const selectImage = (): HTMLImageElement => {
   const element = document.createElement("img");
-  element.setAttribute("src", imageBase + imageList[imageIndex]);
+  element.setAttribute(
+    "src",
+    imageBase + transformation + imageList[imageIndex]
+  );
   element.setAttribute("class", "img-fluid");
 
   return element;
